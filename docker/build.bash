b178149d1c39f621752510ad3cd047f98f43e197
Dávid Juhász <david.juhasz@tuwien.ac.at>
2018-02-28 15:04:52 +0100

    Fix docker resources for odroidxu3-3.10.y
      - Adjust Git clone command to checkout the actual branch of sparta
       instead of master.
    
      - Line endings in script build.bash are not compatible with Linux, use
        dos2unix to convert them in the Linux container.
    
      - Make build processes use multiple threads.
    
      - Build all available sparta targets.
    
      - Remove running container from Docker hub as the image is created
        differently in this branch.

--- docker/build.bash
+++ docker/build.bash
@@ -1,6 +1,6 @@
 #!/bin/bash
 # build the odroid linux image
 
-make ARCH=arm CROSS_COMPILE=arm-linux-gnueabihf- odroidxu3_defconfig
-make -j8 ARCH=arm CROSS_COMPILE=arm-linux-gnueabihf-
-make -j8 ARCH=arm CROSS_COMPILE=arm-linux-gnueabihf- modules
+make -j4 ARCH=arm CROSS_COMPILE=arm-linux-gnueabihf- odroidxu3_defconfig
+make -j4 ARCH=arm CROSS_COMPILE=arm-linux-gnueabihf-
+make -j4 ARCH=arm CROSS_COMPILE=arm-linux-gnueabihf- modules
